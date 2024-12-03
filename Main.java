da86095f6d59d84e88adb2c5a8c9fb785553f131
Till Heinrichs <till@die-heinrichs.net>
2024-12-03 08:56:33 +0100

    made output readable

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main{
     public static void main(String[] args) {
+        System.out.println("Day 01:");
         (new Day01()).solve();
+        System.out.println("\nDay 02:");
         (new Day02()).solve();
+        System.out.println("\nDay 03:");
         (new Day03()).solve();        
     }
 }
