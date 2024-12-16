7975019423f8f215997aeddcff077f7c8ce821e5
Till Heinrichs <till@die-heinrichs.net>
2024-12-16 13:22:00 +0100

    d5

--- code/Day05.java
+++ code/Day05.java
@@ -82,7 +82,51 @@ public class Day05 extends Template {
 
     @Override
     protected String solvePart2() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int totalMiddlePageNumber = 0;
+        int[] col1 = this.part1.get(0);
+        int[] col2 = this.part1.get(1);
+
+        for (int[] update : this.part2) {
+            boolean isOrdered = true;
+
+            for (int i = 0; i < col1.length; i++) {
+                int pos1 = findPosition(update, col1[i]);
+                int pos2 = findPosition(update, col2[i]);
+                if (pos1 != -1 && pos2 != -1 && pos1 > pos2) {
+                    isOrdered = false;
+                    break;
+                }
+            }
+
+            if (!isOrdered) {
+                int[] correctedUpdate = correctUpdate(update, col1, col2);
+                totalMiddlePageNumber += correctedUpdate[correctedUpdate.length / 2];
+            }
+        }
+
+        return String.valueOf(totalMiddlePageNumber);
     }
 
+    private int[] correctUpdate(int[] update, int[] col1, int[] col2) {
+        List<Integer> sorted = new ArrayList<>();
+        List<Integer> remaining = new ArrayList<>();
+        for (int value : update) {
+            remaining.add(value);
+        }
+
+        for (int k = 0; k < col1.length; k++) {
+            int pos1 = findPosition(remaining.stream().mapToInt(Integer::intValue).toArray(), col1[k]);
+            int pos2 = findPosition(remaining.stream().mapToInt(Integer::intValue).toArray(), col2[k]);
+
+            if (pos1 != -1 && pos2 != -1 && pos1 > pos2) {
+                sorted.add(col1[k]);
+                sorted.add(col2[k]);
+                remaining.remove((Integer) col1[k]);
+                remaining.remove((Integer) col2[k]);
+            }
+        }
+
+        sorted.addAll(remaining);
+        return sorted.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
